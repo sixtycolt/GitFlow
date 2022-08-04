3a5d2c1c19604a33d488532d46b3ff5f45e0e021
sixtycolt <sixty.colt@gmail.com>
2022-08-05 00:25:51 +0300

    Initial Commit

---
+++ iOS7-HW10 - Finikopulo Vladimir/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS7-HW10 - Finikopulo Vladimir
+//
+//  Created by PoisonMacbookPro on 05.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
